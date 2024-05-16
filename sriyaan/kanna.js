5f00d8f360c91586c0a057ea83c49656dbc10fad
sureshbandi12 <sureshyadav2243@gmail.com>
2024-05-16 23:52:03 +0530

    html commit

---
+++ sriyaan/kanna.js
@@ -0,0 +1,16 @@
+let weeknum=1;
+if(weeknum==1){
+    console.log('sunday')
+}else if(weeknum==2){
+    console.log('monday')
+}else if(weeknum==3){
+    console.log('tuesday')
+}else if(weeknum==4){
+    console.log('wednesday')
+}else if(weeknum==5){
+    console.log('thursday')
+}else if(weeknum==6){
+    console.log('friday')
+}else if(weeknum==7){
+    console.log('saturday')
+}
