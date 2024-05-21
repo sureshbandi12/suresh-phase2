574be551b2dbcef9b27580ca9d8b568fef450b24
sureshbandi12 <sureshyadav2243@gmail.com>
2024-05-21 14:03:39 +0530

    first

--- dhoni/mahi.js
+++ dhoni/mahi.js
@@ -1,10 +1,10 @@
 let a=10;
-let b=20;
-let c=30;
-if(a>b && a>c){
-    console.log("a is greattest")
-}else if(b>a && b>c){
-    console.log("b is greattest")
-}else {
-    console.log("c is greatest")
+let b=9;
+let c=15;
+if(a<b && a<c){
+    console.log(" a is smallest")
+}else if(b<a && b<c){
+    console.log("b is smallest")
+}else{
+    console.log("c is smllest")
 }
