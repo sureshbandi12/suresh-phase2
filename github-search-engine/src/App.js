9077228ebc131c7401f1f8625ae1c0c5905d0338
sureshbandi12 <sureshyadav2243@gmail.com>
2024-06-10 14:57:40 +0530

    react commit

---
+++ github-search-engine/src/App.js
@@ -0,0 +1,24 @@
+import Header from "./Header.js";
+import Footer from "./Footer.js";
+import   "./App.css"
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <main>
+      <h1>React Tutorial</h1>
+      <p>React is a JavaScript library for building user interfaces.</p>
+      <p>React is a JavaScript library for building user interfaces.</p>
+      <p>React allows us to create reusable UI components.</p>
+
+
+      </main>
+    <Footer/>
+    </div>
+
+  );
+}
+
+export default App;
---
+++ github-search-engine/src/Footer.js
@@ -0,0 +1,33 @@
+import React from "react"
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h4>About Us</h4>
+        <ul>
+          <li><a href="#our-mission">Our Mission</a></li>
+          <li><a href="#team">Team</a></li>
+          <li><a href="#history">History</a></li>
+        </ul>
+      </div>
+      <div>
+        <h4>Contact Us</h4>
+        <ul>
+          <li><a href="#contact-form">Contact Form</a></li>
+          <li><a href="#locations">Locations</a></li>
+          <li><a href="#support">Support</a></li>
+        </ul>
+      </div>
+      <div>
+        <h4>Resources</h4>
+        <ul>
+          <li><a href="#guides">Guides</a></li>
+          <li><a href="#tutorials">Tutorials</a></li>
+          <li><a href="#webinars">Webinars</a></li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
